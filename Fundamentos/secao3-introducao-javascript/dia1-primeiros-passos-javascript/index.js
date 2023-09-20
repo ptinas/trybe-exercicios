ddf51e3c556c2aa8c508b9e4c6cb75c90f8540c7
Pâmela <pameela_cristina@hotmail.com>
2023-09-20 17:52:54 -0300

    Exercícios 1 e 2

---
+++ Fundamentos/secao3-introducao-javascript/dia1-primeiros-passos-javascript/index.js
@@ -0,0 +1,21 @@
+// Adição (num1 + num2)
+// Subtração (num1 - num2)
+// Multiplicação (num1 * num2)
+// Divisão (num1 / num2)
+// Módulo (num1 % num2)
+
+// let num1 = 80
+// let num2 = 40
+
+// console.log(num1 % num2);
+
+// Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas variáveis com os valores que serão comparados.
+// let ageJuliana = 70;
+// let ageKaren = 40;
+
+// if (ageJuliana > ageKaren) {
+ //   message = "Juliana é mais velha."
+// } else {
+//    message = "Karen é mais velha."
+//}
+//console.log(message);

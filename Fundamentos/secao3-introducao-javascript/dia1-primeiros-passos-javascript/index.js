95019e010f0cb63d0f13536ca561554bcf5f0194
Pâmela <pameela_cristina@hotmail.com>
2023-09-20 18:26:10 -0300

    Exercício 3 e 4

--- Fundamentos/secao3-introducao-javascript/dia1-primeiros-passos-javascript/index.js
+++ Fundamentos/secao3-introducao-javascript/dia1-primeiros-passos-javascript/index.js
@@ -1,21 +1,32 @@
-// Adição (num1 + num2)
-// Subtração (num1 - num2)
-// Multiplicação (num1 * num2)
-// Divisão (num1 / num2)
-// Módulo (num1 % num2)
+// Utilize if/else para escrever um código que retorne o maior de três números. Defina, no começo do seu código, três variáveis com os valores que serão comparados.
 
-// let num1 = 80
-// let num2 = 40
+// let Maria = 60;
+// let Fernando = 80;
+// let Joana = 80;
+// let message
 
-// console.log(num1 % num2);
-
-// Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas variáveis com os valores que serão comparados.
-// let ageJuliana = 70;
-// let ageKaren = 40;
-
-// if (ageJuliana > ageKaren) {
- //   message = "Juliana é mais velha."
+//  if (Maria > Fernando && Joana) {
+//    message = "Maria possui mais maçãs!"
+// } else if (Fernando > Joana && Maria) {
+//    message = "Fernando possui mais maçãs!"
+// } else if (Joana > Fernando && Maria) {
+//    message = "Joana possui mais maçãs!"
 // } else {
-//    message = "Karen é mais velha."
-//}
-//console.log(message);
+//    message = "Eles dividiram as maçãs entre si em partes iguais!"
+// }
+
+// console.log(message);
+
+// Utilize if...else para escrever um código que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem 
+// os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, você deve retornar uma mensagem de erro.
+
+let angulo1 = 30;
+let angulo2 = 80;
+let angulo3 = 60;
+let message
+
+if (angulo1 + angulo2 + angulo3 === 180) {
+    console.log(true);
+} else {
+    console.log(false)
+}

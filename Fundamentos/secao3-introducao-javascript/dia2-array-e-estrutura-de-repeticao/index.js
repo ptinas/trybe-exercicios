c8505f5c6228e42eef0b0889cf20cf1770d2d049
Pâmela <pameela_cristina@hotmail.com>
2023-09-23 17:05:34 -0300

    Exercícios

--- Fundamentos/secao3-introducao-javascript/dia2-array-e-estrutura-de-repeticao/index.js
+++ Fundamentos/secao3-introducao-javascript/dia2-array-e-estrutura-de-repeticao/index.js
@@ -1,51 +1,30 @@
-// Percorra o array imprimindo todos os valores contidos nele com a função console.log().
-//  Some todos os valores contidos no array e imprima o resultado.
-// Calcule e imprima a média aritmética dos valores contidos no array. A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
-// Com base no código que acabou de gerar, referente ao cálculo da média aritmética, faça com que: caso o valor final seja maior que 20, imprima a mensagem “O valor da média aritmética é maior 
-// que 20”; e, caso não seja maior que 20, imprima a mensagem “O valor da média aritmética é menor ou igual a 20”.
-// Utilizando for, descubra o maior valor contido no array e imprima-o.
-// Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “Nenhum valor ímpar encontrado”.
+//Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra, como a palavra “banana” para “ananab”. Utilize a string abaixo como exemplo. 
+// Depois, troque-a por outras para verificar se seu algoritmo está funcionando corretamente.
+// let word = 'tryber'.split("").reverse();
+// console.log(word);
 
-const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let result = 0;
-let media = 0;
-let message
-let higherNumber = numbers[0];
-let modulo = 0;
+// Escreva um algoritmo que, dado um valor n, sendo n > 1, imprima na tela um quadrado feito de asteriscos de tamanho n.
 
-for (let index = 0; index < numbers.length; index += 1) {
-    console.log(numbers[index]);
-    result += numbers[index];
-    media = result / numbers.length;
+// let numero = 5;
+// let asterisco = "*";
+// let linha = " ";
+
+// for (let index = 0; index < numero; index +=1) {
+//    linha = linha + asterisco;
+// }
+
+// for (let index = 0; index < numero; index +=1) {
+//  console.log(linha);
+// }
+
+// Faça o mesmo algoritmo que antes, mas de modo que imprima um triângulo retângulo com 5 asteriscos de base.
+
+let numero = 5;
+let asterisco = "*";
+let linha = " ";
+
+for (let index = 0; index <= numero; index +=1) {
+    linha = linha + asterisco;
+    console.log(linha);
+
 }
-
-for (let index = 1; index < numbers.length; index += 1) {
-    if (numbers[index] > higherNumber) {
-        higherNumber = numbers[index];
-    }
-};
-
-if (media > 20) {
-    message = "O valor da média aritmética é maior que 20.";
-} else {
-    message = "O valor da média aritmética é menor ou igual a 20.";
-}
-
-for (index = 0; index < numbers.length; index += 1) {
-    if (numbers[index] % 2 !== 0) {
-        modulo +=1;
-    }
-}
-
-if (modulo === 0) {
-    console.log("Nenhum valor ímpar encontrado")
-} else {
-    console.log(`São ${modulo} números ímpares.`);
-}
-
-
-
-console.log(`A soma de todos os valores é: ${result}.`);
-console.log(`A media aritmética dos valores é: ${media}.`);
-console.log(message);
-console.log(`O maior número é: ${higherNumber}.`);

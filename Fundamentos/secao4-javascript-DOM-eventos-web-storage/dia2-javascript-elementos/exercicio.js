dfda66a7055aa5ebd60be6991fbc6f856d58717f
Pâmela <pameela_cristina@hotmail.com>
2023-10-03 19:25:01 -0300

    Bônus exercício

--- Fundamentos/secao4-javascript-DOM-eventos-web-storage/dia2-javascript-elementos/exercicio.js
+++ Fundamentos/secao4-javascript-DOM-eventos-web-storage/dia2-javascript-elementos/exercicio.js
@@ -2,6 +2,7 @@
 let elementBody = document.getElementsByTagName('body');
 let textTag = document.createElement('h1');
 textTag.innerHTML = 'TrybeTrip - Agência de Viagens'
+textTag.className = 'title';
 elementBody[0].appendChild(textTag);
 
 let tagMain = document.createElement('main');
@@ -10,6 +11,7 @@ elementBody[0].appendChild(tagMain);
 
 let tagSection = document.createElement('section');
 tagSection.className = 'center-content';
+tagSection.style.backgroundColor = '#a5be00';
 tagMain.appendChild(tagSection);
 
 let tagParagraph = document.createElement('p');
@@ -19,9 +21,15 @@ tagSection.appendChild(tagParagraph);
 let leftSection = document.createElement('section');
 leftSection.className = 'left-content';
 tagMain.appendChild(leftSection);
+const rmLeftContent = document.getElementsByClassName('left-content')[0];
+tagMain.removeChild(rmLeftContent);
 
 let rightSection = document.createElement('section');
 rightSection.className = 'right-content';
+rightSection.style.marginRight = '600px';
+rightSection.style.marginLeft = '600px';
+rightSection.style.textAlign = 'center';
+
 tagMain.appendChild(rightSection);
 
 let image = document.createElement('img');
@@ -48,11 +56,12 @@ for (let index = 0; index < numberList.length; index += 1) {
     list.appendChild(li);
 }
 rightSection.appendChild(list);
+list.lastChild.remove();
+list.lastChild.remove();
 
 for (let index = 0; index < 3; index += 1) {
     let tagH3 = document.createElement('h3');
     tagH3.innerHTML = index;
     tagMain.appendChild(tagH3);
+    tagH3.className = 'description';
 }
-
-

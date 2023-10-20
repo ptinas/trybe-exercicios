563a52341fec0131c8255f8b6a5cd569517e5ba4
Pâmela <pameela_cristina@hotmail.com>
2023-10-20 14:35:56 -0300

    Iniciando exercício

---
+++ Fundamentos/segundo-periodo/secao6-introducao-javascript-ES6-testes-unitarios/dia2-primeiros-passos-em-jest/script.js
@@ -0,0 +1,24 @@
+// removeItem.js
+
+function removeItem(arr, item) {
+    let newArr = [];
+    for (let index = 0; index < arr.length; index += 1) {
+      if (item !== arr[index]) {
+        newArr.push(arr[index]);
+      }
+    }
+    return newArr;
+  }
+
+  
+  /*
+
+  Copie a função já implementada e desenvolva os testes. Separe a função e o teste em arquivos diferentes para evitar qualquer tipo de problema.
+
+A função removeItem(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item, caso ele exista no array.
+
+Verifique se a chamada removeItem([1, 2, 3, 4], 3) retorna o array esperado.
+Verifique se a chamada removeItem([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4].
+Verifique se a chamada removeItem([1, 2, 3, 4], 5) retorna o array esperado.
+
+*/

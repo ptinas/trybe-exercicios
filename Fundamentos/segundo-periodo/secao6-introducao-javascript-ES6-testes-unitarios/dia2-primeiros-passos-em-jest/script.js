95273d6135dcc39266d5d99d535c56aff512504f
Pâmela <pameela_cristina@hotmail.com>
2023-10-20 23:08:46 -0300

    Exercício 4 feito

--- Fundamentos/segundo-periodo/secao6-introducao-javascript-ES6-testes-unitarios/dia2-primeiros-passos-em-jest/script.js
+++ Fundamentos/segundo-periodo/secao6-introducao-javascript-ES6-testes-unitarios/dia2-primeiros-passos-em-jest/script.js
@@ -1,4 +1,4 @@
-// removeItem.js
+// removeItem
 
 function removeItem(arr, item) {
     let newArr = [];
@@ -10,7 +10,7 @@ function removeItem(arr, item) {
     return newArr;
   }
 
-  // myFizzBuzz.js
+  // myFizzBuzz
 
 function myFizzBuzz(num) {
     if (typeof num !== 'number') return false;
@@ -19,6 +19,48 @@ function myFizzBuzz(num) {
     if (num % 5 === 0) return 'buzz';
     return num;
   }
-console.log(myFizzBuzz(15));
 
-module.exports = {removeItem, myFizzBuzz}
+  // encodeDecode
+
+const mapString = (objectMap, string) => {
+    const splitString = string.split('');
+    const mappedArray = [];
+  
+    for (let index = 0; index < splitString.length; index += 1) {
+      const character = splitString[index];
+      const mappedValue = objectMap[character];
+      
+      if (mappedValue) {
+        mappedArray.push(mappedValue);
+      } else {
+        mappedArray.push(character);
+      }
+    }
+  
+    return mappedArray.join('');
+  }
+  
+  const encode = (string) => {
+    const map = {
+      a: 1,
+      e: 2,
+      i: 3,
+      o: 4,
+      u: 5,
+    };
+    return mapString(map, string);
+  }
+  
+  const decode = (string) => {
+    const map = {
+      1: 'a',
+      2: 'e',
+      3: 'i',
+      4: 'o',
+      5: 'u',
+    }
+    return mapString(map, string);
+  }
+
+console.log(decode('1'))
+module.exports = {removeItem, myFizzBuzz, encode, decode}
--- Fundamentos/segundo-periodo/secao6-introducao-javascript-ES6-testes-unitarios/dia2-primeiros-passos-em-jest/script.test.js
+++ Fundamentos/segundo-periodo/secao6-introducao-javascript-ES6-testes-unitarios/dia2-primeiros-passos-em-jest/script.test.js
@@ -1,4 +1,4 @@
-const { removeItem, myFizzBuzz } = require('./script')
+const { removeItem, myFizzBuzz, encode, decode } = require('./script')
 
 
 describe('A função removeItem recebe um array arr e retorna uma cópia desse array sem o elemento item, caso ele exista no array.', () => {
@@ -12,8 +12,7 @@ describe('A função removeItem recebe um array arr e retorna uma cópia desse a
     test('Testa se a chamada removeItem([1, 2, 3, 4], 5) retorna o array esperado.', () => {
         expect(removeItem([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4])
     })
-}
-)
+});
 
 describe('A função myFizzBuzz recebe um número num como parâmetro.', () => {
     test('Caso num seja um número divisível por 3 e 5, a função retorna "fizzbuzz"', () => {
@@ -22,7 +21,7 @@ describe('A função myFizzBuzz recebe um número num como parâmetro.', () => {
     test('Caso num seja um número divisível apenas por 3, a função retorna "fizz"', () => {
         expect(myFizzBuzz(9)).toBe('fizz');
     })
-   test('Caso num seja um número divisível apenas por 5, a função retorna "buzz"', () => {
+    test('Caso num seja um número divisível apenas por 5, a função retorna "buzz"', () => {
         expect(myFizzBuzz(10)).toBe('buzz');
     })
     test('Caso num seja um número que não é divisível nem por 3 nem por 5, a função retorna o próprio número num', () => {
@@ -33,7 +32,62 @@ describe('A função myFizzBuzz recebe um número num como parâmetro.', () => {
     })
 });
 
-/*
 
+describe('Testa encode', () => {
+    test('Testa se encode é uma função.', () => {
+        expect(typeof encode).toBe('function');
+    })
+    test('Testa se encode recebe aeiou e converte em 12345.', () => {
+        expect(encode('aeiou')).toBe('12345');
+    })
+    test('Testa se a string retornada por encode tem o mesmo número de caracteres que a string parâmetro', () => {
+        expect(encode('3f5ae').length).toEqual(5);
+    })
+    test('Testa se encode recebe a letra a e retorna 1', () => {
+        expect(encode('a')).toBe('1');
+    })    
+    test('Testa se encode recebe a letra e e retorna 2', () => {
+        expect(encode('e')).toBe('2');
+    })
+    test('Testa se encode recebe a letra i e retorna 3', () => {
+        expect(encode('i')).toBe('3');
+    })
+    test('Testa se encode recebe a letra o e retorna 4', () => {
+        expect(encode('o')).toBe('4');
+    })
+    test('Testa se encode recebe a letra u e retorna 5', () => {
+        expect(encode('u')).toBe('5');
+    })
+});
+
+describe('Testa decode', () => {
+    test('Testa se decode é uma função.', () => {
+        expect(typeof decode).toBe('function');
+    })
+    test('Testa se decode recebe 12345 e converte em aeiou.', () => {
+        expect(decode('12345')).toMatch('aeiou');
+    })
+    test('Testa se a string retornada por decode tem o mesmo número de caracteres que a string parâmetro', () => {
+        expect(decode('aefo3').length).toEqual(5);
+    })
+    test('Testa se decode recebe a string 1 e retorna a', () => {
+        expect(decode('1')).toMatch('a');
+    })    
+    test('Testa se decode recebe a string 2 e retorna b', () => {
+        expect(decode('2')).toMatch('e');
+    })
+    test('Testa se decode recebe a string 3 e retorna b', () => {
+        expect(decode('3')).toMatch('i');
+    })
+    test('Testa se decode recebe a string 4 e retorna b', () => {
+        expect(decode('4')).toMatch('o');
+    })
+    test('Testa se decode recebe a string 5 e retorna b', () => {
+        expect(decode('5')).toMatch('u');
+    })
+});
+
+
+/*
 
 */
